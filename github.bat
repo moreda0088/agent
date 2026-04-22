a7356f7971d57be5183f0386d7db97401a2a29b9
moreda0088 <moreda0088@gmail.com>
2026-04-22 22:39:11 +0200

    AgentFlow - Agentic Economy on Arc Hackathon

--- github.bat
+++ github.bat
@@ -38,7 +38,7 @@ echo  [5/5] Pushing to GitHub...
 git remote remove origin >nul 2>&1
 git remote add origin https://github.com/moreda0088/agent.git
 git branch -M main
-git push -u origin main
+git push -u origin main -f
 
 echo.
 echo  ====================================================
